ad982365c98e02722b4b5de9f0e63c64fcca8c10
Marco Ricupero <marco.ricupero@gmail.com>
2019-12-09 23:24:28 +0100

    New Config: Rollup and package scripts

--- rollup.config.js
+++ rollup.config.js
@@ -1,71 +1,102 @@
 import rollupPluginTs from "@wessberg/rollup-plugin-ts";
-import pkg from './package.json';
-import tsconfig from './tsconfig.json';
-import { terser } from 'rollup-plugin-terser';
-import builtins from 'rollup-plugin-node-builtins';
-import license from 'rollup-plugin-license';
-import path from 'path';
-
+import pkg from "./package.json";
+import tsconfig from "./tsconfig.json";
+import { terser } from "rollup-plugin-terser";
+import builtins from "rollup-plugin-node-builtins";
+import license from "rollup-plugin-license";
+import path from "path";
 
 const baseConf = {
-    input: 'src/diacritic-remover.ts',
     external: [
         ...Object.keys(pkg.dependencies || {}),
-        'i18n/all'
+        "i18n/all",
     ],
+    input: "src/diacritic-remover.ts",
+    output: {
+        name: "DiacriticRemover",
+        sourcemap: true,
+    },
     plugins: [
-        //rollupPluginTs({}),
-        builtins(),
-        terser(),
         license({
-            cwd: __dirname,
-            banner: {        
+            banner: {
                 content: {
-                  file: path.join('.', 'src/banner'),
+                  file: path.join(".", "src/banner"),
                 },
-              },        
+            },
+            cwd: __dirname,
         }),
     ],
-} 
+};
+
+const rollupBrowserConf = rollupPluginTs({
+    tsconfig: {
+        ...tsconfig.compilerOptions,
+        declaration: false,
+        module: "iife",
+        target: "ES2015",
+    },
+});
+const rollupModuleConf = rollupPluginTs({
+    tsconfig: {
+        ...tsconfig.compilerOptions,
+        declaration: false,
+        module: "ESNext",
+        target: "ESNext",
+    },
+});
 
 export default [
     {
         ...baseConf,
-        output: [
-            {
-                file: pkg.main,
-                format: 'cjs',
-                sourcemap: true
-            }
-        ],
+        output: {
+            ...baseConf.output,
+            file: pkg.main,
+            format: "cjs",
+        },
         plugins: [
+            builtins(),
             rollupPluginTs({}),
-            ...baseConf.plugins
-        ]
+            ...baseConf.plugins,
+        ],
     },
     {
         ...baseConf,
-        output: [
-            {
-                file: pkg.module,
-                format: 'es',
-                sourcemap: true
-            },
-            {
-                file: 'dist/diacritic-remover.bundle.min.js',
-                format: 'iife',
-                name: 'DiacriticRemover',
-                sourcemap: true
-            }
-        ],
+        output: {
+            ...baseConf.output,
+            file: pkg.module,
+            format: "esm",
+        },
         plugins: [
-            rollupPluginTs({
-                tsconfig: {
-                    ...tsconfig.compilerOptions,
-                    declaration: false
-                }
-            }),
-            ...baseConf.plugins
-        ]
-    }
+            builtins(),
+            rollupModuleConf,
+            ...baseConf.plugins,
+        ],
+    },
+    {
+        ...baseConf,
+        output: {
+            ...baseConf.output,
+            file: "dist/diacritic-remover.bundle.js",
+            format: "iife",
+        },
+        plugins: [
+            builtins(),
+            rollupBrowserConf,
+            ...baseConf.plugins,
+        ],
+    },
+    {
+        ...baseConf,
+        output: {
+            ...baseConf.output,
+            file: "dist/diacritic-remover.bundle.min.js",
+            format: "iife",
+        },
+        plugins: [
+            builtins(),
+            rollupBrowserConf,
+            terser(),
+            ...baseConf.plugins,
+        ],
+    },
 ];
