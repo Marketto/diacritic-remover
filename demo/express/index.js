0daa062ae635d72f60cf88a41d04b717ea2c14cd
Marco Ricupero <marco.ricupero@gmail.com>
2019-12-10 22:32:09 +0100

    Node.js/Express Demo

---
+++ demo/express/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const DiacriticRemover = require("@marketto/diacritic-remover");
+const diacriticRemover = new DiacriticRemover();
+const server = express();
+
+server.get("/diacritic-remover/:text", (req, res) => {
+    res.send(diacriticRemover.replace(req.params.text));
+});
+
+server.listen(3000, () => console.log("Running on port 3000"));
